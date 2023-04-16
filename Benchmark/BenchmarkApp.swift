ff72fb23f5d076ed793a7888dbb9098641116ec5
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-04-16 12:28:16 -0400

    Initial Commit

---
+++ Benchmark/BenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchmarkApp.swift
+//  Benchmark
+//
+//  Created by Jonas Eschmann on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
